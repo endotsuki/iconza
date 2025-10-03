94ff9b45c3133cad2ffc4458e036f2211fd6b59d
Onimuxha <cheathekm@gmail.com>
2025-10-03 09:30:01 +0700

    Add sitemap generator script

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { handleDemo } from "./routes/demo";
-import { handleSitemap } from "../script/sitemap";
+import { handleSitemap } from "../script/generate-sitemap";
 
 export function createServer() {
   const app = express();
