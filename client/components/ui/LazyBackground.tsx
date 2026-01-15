e67beb942b9d0cdaed5bf285d731356841f62bdc
Cheathek <cheathekm@gmail.com>
2026-01-15 23:03:38 +0700

    update file font

--- client/components/ui/LazyBackground.tsx
+++ client/components/ui/LazyBackground.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils';
 import { Suspense, lazy } from 'react';
 
 // const ShootingStars = lazy(() => import('./shooting-stars').then(mod => ({ default: mod.ShootingStars })));
@@ -10,8 +11,20 @@ export default function LazyBackground() {
       <div className="pointer-events-none fixed inset-0 -z-10 select-none">
         {/* <ShootingStars />
         <StarsBackground /> */}
-        <HexagonBackground hexagonSize={80} hexagonMargin={3} className="absolute inset-0" />
-      </div>
+        {/* <HexagonBackground hexagonSize={80} hexagonMargin={3} className="absolute inset-0" /> */}
+        <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)] dark:bg-black"></div>
+    </div>
+    </div>
     </Suspense>
   );
 }
